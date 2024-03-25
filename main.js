66a51b225eb5fcdc13636303ce617e431c5bb487
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-25 23:43:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict";
+//      Operators
+let num = 45;
+let num1 = 42;
+let result = num + num1;
+console.log(result);
+let a = 23;
+let b = 45;
+let result1 = a + b;
+console.log(result1);
+let b1 = 34;
+let b2 = 65;
+let result2 = b2 - b1;
+console.log(result2);
+let x = -45 + -34 + -1 + 22; // -45 -34  -1  + 22
+let y = -23 + 3 + 1 + -9; // -23 + 3  +1  -9
+console.log(x + y);
+let m = -24 + 12 + -6 + 8; // -24 +12 -6 +8
+let n = -23 + -6 + 13 + -0; // -23 -6 +13 -0            
+console.log(m - n);
+let t = 4;
+let r = 2;
+console.log(t % r);
+const u = 54;
+const v = 43;
+console.log(u % v);
+const i = 34;
+const j = 19;
+console.log(i % j);
